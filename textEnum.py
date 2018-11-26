fe05a2c0972b2a5bd896be835e1c114a6e0d82b3
Nastassja Riemermann <tsukikage@gmail.com>
2018-11-25 23:54:27 -0600

    capitalized chapter list constant; added comments

--- textEnum.py
+++ textEnum.py
@@ -8,15 +8,20 @@ Written by Nastassja Riemermann (tsukikage)
 18-11-24
 """
 
-ch_ls = [(1,68),(2,45), (3,49), (4,36), (5,87), (6,45), (7,63), (8,55), (9,46)]
-print_ls = []
+# list of chapters and no. of examples, etc. (to be edited by user as needed)
+CH_LS = [(1,68),(2,45), (3,49), (4,36), (5,87), (6,45), (7,63), (8,55), (9,46)]
 
-for ch_tup in ch_ls:
+# Generate enumeration of examples, etc., for each chapter which will be output.
+# Each chapter is its own string, which is its own item in a list.
+output_ls = []
+for ch_tup in CH_LS:
     new_str = ""
     for idx in range(ch_tup[1]):
         new_str += "{}.{} ".format(ch_tup[0],idx+1)
-    print_ls.append(new_str)
+    output_ls.append(new_str)
 
+# Write the output list to "text enum.txt", with a blank line between each
+# list element (chapter).
 with open('text enum.txt', 'w') as f:
-    for item in print_ls:
+    for item in output_ls:
         f.write("%s\n\n" % item)
