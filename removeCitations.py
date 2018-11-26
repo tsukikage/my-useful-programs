9eeb83128a4554e1d03c5425dbd9912a430ffa8b
Nastassja Riemermann <tsukikage@gmail.com>
2018-11-25 23:51:59 -0600

    rearranged import statements; added comments

--- removeCitations.py
+++ removeCitations.py
@@ -7,18 +7,24 @@ Nastassja Riemermann (tsukikage)
 18-11-25
 """
 
+# input file (to be edited by user as needed)
 IFILE = 'AI history.txt'
 
+# import statements
 import re
+import io
 
+# generate output filename with " CR" appended to the end
 ofile = IFILE[:-4]+' CR.txt'
 
+# read input file
 with open(IFILE, 'r', encoding='utf-8') as fin:
     text = fin.read()
 
+# remove all citations
 citation = re.compile('['+r'[0-9]+'+']')
 text = re.sub(citation, '', text)
 
-import io
+# crete output file (if needed) and write new version without citations
 with io.open(ofile, "w", encoding="utf-8") as f:
     f.write(text)
